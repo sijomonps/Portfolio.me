a2d8eca32fc7e58ba1341568801a882bc149ecde
sijomonps <sijomon700@gmail.com>
2026-05-08 16:04:09 +0530

    meta tag added

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" data-theme="light" suppressHydrationWarning>
+    <html lang="en" data-theme="dark" suppressHydrationWarning>
       <head>
         <script dangerouslySetInnerHTML={{
           __html: `
